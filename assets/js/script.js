ad5a33e55836d9c73b7b6c2569152657589b5421
Hicham Mokaddem <73243340+mokaddemhicham@users.noreply.github.com>
2023-06-25 15:39:08 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const questions = [{
     c: "Une décision",
     d: "Instructions pas à pas utilisées pour résoudre un problème",
     correct: "d",
+    justification: "Un algorithme est une séquence d'instructions précises et ordonnées qui permet de résoudre un problème donné."
 },
 {
     question: "Quelles sont les trois constructions d’algorithme?",
@@ -13,6 +14,7 @@ const questions = [{
     c: "Entrée / Sortie, Décision, Répétition",
     d: "Boucle, Entrée/Sortie, Processus",
     correct: "b",
+    justification: "Les trois constructions d'un algorithme sont la séquence (exécution linéaire des instructions), la sélection (prise de décisions conditionnelles) et la répétition (boucles)."
 },
 {
     question: "Pour répéter une tâche, nous utilisons une ____?",
@@ -21,6 +23,7 @@ const questions = [{
     c: "Boucle",
     d: "Sortie",
     correct: "c",
+    justification: "Pour répéter une tâche, on utilise une boucle, qui permet d'exécuter un bloc d'instructions plusieurs fois jusqu'à ce qu'une condition spécifique soit satisfaite."
 },
 {
     question: "Si ....... Alors ....... Sinon ....... Fin Si vérifier ____?",
@@ -29,6 +32,7 @@ const questions = [{
     c: "Trois conditions",
     d: "Plusieurs conditions",
     correct: "b",
+    justification: "La structure \"Si... Alors... Sinon... Fin Si\" permet de vérifier une seule condition et d'exécuter différentes instructions en fonction du résultat de cette condition."
 },
 {
     question: "Quelle est la fonction principale d'un algorithme ?",
@@ -37,6 +41,7 @@ const questions = [{
     c: "Créer des interfaces graphiques",
     d: "Stocker des données",
     correct: "b",
+    justification: "La fonction principale d'un algorithme est de résoudre un problème spécifique en fournissant une séquence d'instructions claires et logiques."
 },
 {
     question: "Qu'est-ce qu'une variable dans un algorithme ?",
@@ -45,6 +50,7 @@ const questions = [{
     c: "Une condition de contrôle",
     d: "Un type de donnée",
     correct: "a",
+    justification: "Une variable dans un algorithme est utilisée pour stocker temporairement des données ou des valeurs et peut être modifiée au cours de l'exécution de l'algorithme."
 },
 {
     question: "Un organigramme _______?",
@@ -53,6 +59,7 @@ const questions = [{
     c: "Utilise des formes pour aider à organiser un processus",
     d: "Tout les réponses sont vrais",
     correct: "d",
+    justification: "Un organigramme est un type de diagramme qui représente un algorithme, un flux de travail ou un processus. L’organigramme peut également être défini comme une représentation schématique d’un algorithme (approche pas à pas pour résoudre une tâche)."
 },
 {
     question: "Quel est l'objectif principal d'un algorithme ?",
@@ -61,6 +68,7 @@ const questions = [{
     c: "Afficher des résultats à l'écran",
     d: "Créer une interface utilisateur",
     correct: "a",
+    justification: "L'objectif principal d'un algorithme est de fournir une solution efficace et précise à un problème donné."
 }
 ];
 
@@ -76,27 +84,27 @@ var question = document.getElementById("question"),
     countDown = document.querySelector(".countdown"),
     ans_labels = document.querySelectorAll(".ans_label"),
     submit = document.getElementById("submit");
-// load quiz
-var currentQuestion = 0;
-var score = 0;
 var nameOfPlayer = localStorage.getItem("username") ? localStorage.getItem("username") : "";
 var audio = document.getElementById('backgroundQuizMusic');
 var correctSound = document.getElementById('correctSound');
 var wrongSound = document.getElementById('wrongSound');
 var numberOfQuestions = document.getElementById('numberOfQuestions');
-
+var currentQuestion = 0;
+var score = 0;
+var wrongResponses = [];
 
 function shuffleArray(array) {
     return array.sort(function() {
-      return Math.random() - 0.5;
+        return Math.random() - 0.5;
     });
-  }
-  
+}
+
 
 var numberList = [0, 1, 2, 3, 4, 5, 6, 7];
 var shuffledList = shuffleArray(numberList);
 var c__question;
-
+    
+// load quiz
 loadQuiz();
 
 
@@ -110,7 +118,6 @@ function loadQuiz(){
     submit.disabled = false;
     numberOfQuestions.innerHTML = `${++currentQuestion}/${questions.length}`;
     currentQuestion--;
-    //console.log(currentQuestion + " => " + c__question);
     c__question = shuffledList[currentQuestion];
     question.innerHTML = questions[c__question].question;
 
@@ -183,6 +190,7 @@ function checkAnswer(answer){
                 correctSound.pause();
             }, 2000);
         }else{
+            wrongResponses.push(c__question);
             document.getElementById(label).style.color = "red";
             document.getElementById(questions[c__question].correct+'_text').style.color = "green";
             document.getElementById(questions[c__question].correct+'_text').style.fontWeight = "bold";
@@ -222,7 +230,7 @@ function checkTime(){
         let c_question = currentQuestion - 1;
         document.getElementById(questions[shuffledList[c_question]].correct+'_text').style.color = "green";
         document.getElementById(questions[shuffledList[c_question]].correct+'_text').style.fontWeight = "bold";
-
+        wrongResponses.push(shuffledList[c_question]);
         if(currentQuestion < questions.length){
             setTimeout(loadQuiz, 2000);
         }else{
@@ -251,9 +259,28 @@ function displayResult(name){
         icon = 'check';
     }
 
-    finalScore.innerHTML = '<table class="table table-bordered text-center"><thead><tr><th colspan="2">Bonjour ' + name + '</th></tr><tr><th colspan="2"><i class="fa-regular fa-circle-'+ icon +'"></i></th></tr></thead><tr><td>Score</td><td>' + res + '%</td></tr><tr><td>Résultat</td><td>'+ message +'</td></tr></table>';
+    var justifications = '';
+    if(wrongResponses.length != 0){
+        justifications = `<table class="table table-bordered text-center my-2"><thead><tr>
+                            <th>Question</th>
+                            <th>Justification</th>
+                            </tr></thead>`;
+
+        for(let i = 0; i < wrongResponses.length; i++){
+            justifications += `<tr>
+            <td>${questions[wrongResponses[i]].question}</td>
+            <td>${questions[wrongResponses[i]].justification}</td>
+            </tr>`;
+        }
+
+        justifications += '</table>';
+    }
+
+    finalScore.innerHTML = '<table class="table table-bordered text-center"><thead><tr><th colspan="2">Bonjour ' + name + '</th></tr><tr><th colspan="2"><i class="fa-regular fa-circle-'+ icon +'"></i></th></tr></thead><tr><td>Score</td><td>' + res + '%</td></tr><tr><td>Résultat</td><td>'+ message +'</td></tr></table>'+justifications;
+
     submit.innerHTML = 'Recommencer';
     submit.disabled = false;
-    submit.setAttribute('onclick','location.replace("index.html")');
+    submit.setAttribute('onclick','location.replace("../index.html")');
+    submit.classList.add('mb-3');
     
 }
